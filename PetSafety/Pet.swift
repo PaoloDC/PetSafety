b60b6f853bddce26d6c0d9475a30451a37c8776b
decristofaropaolo <decri.paolo@gmail.com>
2018-07-10 10:30:47 +0200

    first commit

---
+++ PetSafety/Pet.swift
@@ -0,0 +1,17 @@
+//
+//  Pet.swift
+//  PetSafety
+//
+//  Created by De Cristofaro Paolo on 10/07/18.
+//  Copyright © 2018 De Cristofaro Paolo. All rights reserved.
+//
+
+import UIKit
+
+class Pet: NSObject {
+    var name: String
+    
+    init(name aName: String) {
+        self.name = aName
+    }
+}
