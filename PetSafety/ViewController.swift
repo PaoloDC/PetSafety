fd32e5bed66288ade2630030a74e628726cc73c2
slambiase6 <totore777@gmail.com>
2018-07-11 12:19:36 +0200

    Importata libreria Eureka

--- PetSafety/ViewController.swift
+++ PetSafety/ViewController.swift
@@ -7,13 +7,43 @@
 //
 
 import UIKit
+import Eureka
 
-class ViewController: UIViewController {
+class ViewController: FormViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+            form +++ Section("Informazioni")
+                <<< TextRow(){ name in
+                    name.title = "Name"
+                    name.placeholder = "Insert pet's name"
+                }
+                <<< ActionSheetRow<String>() { type in
+                    type.title = "Type"
+                    type.selectorTitle = "Peek an pet"
+                    type.options = ["Dog","Cat","Rabbit"]
+                    type.value = "Dog"    // initially selected
+                }
+                <<< TextRow(){ race in
+                    race.title = "Race"
+                    race.placeholder = "Insert pet's race"
+                }
+                <<< DateRow(){ date in
+                    date.title = "Date of birth"
+                    date.value = Date(timeIntervalSinceReferenceDate: 0)
+                }
+                <<< TextRow(){ microchip in
+                    microchip.title = "Microchip ID"
+                    microchip.placeholder = "Insert pet's microchip ID"
+                    }
+                <<< TextRow(){ beacon in
+                    beacon.title = "Beacon ID"
+                    beacon.placeholder = "Insert pet's beacon ID"
+                }
+                
         // Do any additional setup after loading the view, typically from a nib.
     }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '9.0'
+
+target 'PetSafety' do
+  use_frameworks!
+  pod 'Eureka'
+  # Pods for PetSafety
+
+  target 'PetSafetyTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'PetSafetyUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
