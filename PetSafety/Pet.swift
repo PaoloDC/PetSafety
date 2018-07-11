5184cea36cdf790a0ab7e39267b69e0270596d6b
fm6391 <filippo91@gmail.com>
2018-07-11 16:30:54 +0200

    aggiunte nuove voci a pet e sulla view

--- PetSafety/Pet.swift
+++ PetSafety/Pet.swift
@@ -11,10 +11,20 @@ import UIKit
 class Pet: NSObject {
     var name: String
     var race: String
+    var type: String
+    var photo: String
+    var birthDate: Date
+    var microchipID: String
+    var beaconUUID: String
     
-    init(name: String, race: String) {
+    init(name: String, race: String, type: String, photo: String, birthDate: Date, microchipID: String, beaconUUID: String) {
         self.name = name
         self.race = race
+        self.type = type
+        self.photo = photo
+        self.birthDate = birthDate
+        self.microchipID = microchipID
+        self.beaconUUID = beaconUUID
     }
 }
  
